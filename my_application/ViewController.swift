ab09493e84993263f400e4f84eea1586c25f9e87
user913027 <user913027@74-80-245-8.ic2net.net>
2018-08-21 19:39:03 -0700

    Initial Commit

---
+++ my_application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my_application
+//
+//  Created by user913027 on 8/21/18.
+//  Copyright © 2018 user913027. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
